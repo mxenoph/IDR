fb999fc39cfc3336c31121a413641ca285c18d27
Maria Xenophontos <mx222@cam.ac.uk>
2015-04-21 15:12:02 +0100

    added plots from ggplot2

--- batch-consistency-plot.r
+++ batch-consistency-plot.r
@@ -17,9 +17,9 @@ output.file.prefix <- args[2] # file name for plot, generated from script at the
 df.txt <- 10
 ntemp <- as.numeric(npair)
 saved.file.prefix <- list() # identifier of filenames that contain the em and URI results
+
 path_to_functions = system('which functions-all-clayton-12-13.r', intern=T)
 source(path_to_functions)
-#source("functions-all-clayton-12-13.r")
 
 uri.list <- list()
 uri.list.match <- list()
@@ -65,5 +65,49 @@ plot.uri.group(uri.list.match, NULL, file.name=NULL, c(1:npair), title.txt="matc
 plot.ez.group(ez.list, plot.dir=NULL, file.name=NULL, legend.txt=c(1:npair), y.lim=c(0, 0.6))
 plot(0, 1, type="n", xlim=c(0,1), ylim=c(0,1), xlab="", ylab="", xaxt="n", yaxt="n") # legends
 legend(0, 1, legend.txt, cex=0.6)
+
+dev.off()
+
+# plots also the r standard plots, should really write a function to gather the data
+# rather than hacking the idr functions
+pdf(paste(output.file.prefix, "-ggplot.pdf", sep=""), paper='a4')
+plot_diagnostics = function(points, lines, slopes, title=NULL){# {{{
+    library(ggplot2)
+    # Setting colour=NA outside the aes removes the black border from points
+    p = ggplot(data=points, aes(x=tv, y=uri, fill=comparison)) + geom_point(pch=21, size=2.5, colour=NA)
+    # x axis is all significant peaks and y is the common in each comparison so max(x) is the limit for both axes
+    p = p + geom_abline(intercept=0, slope=1, linetype="dotted")
+    p = p + xlab('# significant peaks') + ylab('# peaks in common') + theme_bw() + theme(legend.key=element_blank())
+    p = p + geom_line(data=lines, aes(x=x, y=y, colour=comparison), size=2.5) + ylim(0, max(points[,1:2]))
+    legend = g_legend(p)
+    p = p + ggtitle(title) + theme(legend.position="none")
+
+    y = ggplot(data=slopes, aes(x=x, y=y, colour=comparison)) + geom_line(size=2.5)
+    y = y + geom_abline(intercept=1, slope=0, linetype="dotted")
+    y = y + xlab('# significant peaks') + ylab('slope')
+    y = y + theme_bw() + theme(legend.position="none") + ylim(0, 1.5) + ggtitle(title)
+
+    return(list('p' = p, 'y' = y, 'leg'=legend))
+}
+# }}}
+
+grobs= list()
+summarised_data = plot.uri.group(uri.list, NULL, file.name=NULL, legend.txt, title.txt="all peaks")
+x = plot_diagnostics(summarised_data$data_points, summarised_data$data_lines, summarised_data$data_slopes, 'all peaks')
+grobs[[1]] = x[['p']]
+grobs[[2]] = x[['y']]
+
+summarised_data = plot.uri.group(uri.list.match, NULL, file.name=NULL, legend.txt, title.txt="matched peaks")
+x = plot_diagnostics(summarised_data$data_points, summarised_data$data_lines, summarised_data$data_slopes, 'matched peaks')
+grobs[[3]] = x[['p']]
+grobs[[4]] = x[['y']]
+
+
+summarised_data = plot.ez.group(ez.list, plot.dir=NULL, file.name=NULL, legend.txt=legend.txt, y.lim=c(0, 0.6))
+grobs[[5]] = ggplot(summarised_data, aes(x=n.plot, y=IDR.plot, fill=comparison)) + geom_point(pch=21, colour=NA) + geom_line() + theme_bw() + theme(legend.position="none")
+grobs[[6]] = x[['leg']]
+
+library(gridExtra)
+do.call('grid.arrange', c(grobs, ncol=2, main=output.file.prefix))
 
 dev.off()
